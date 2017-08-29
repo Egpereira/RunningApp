8af62a40d606b2af730c7ee47e145437b7623ce4
Unknown <egpereira05@gmail.com>
2017-08-29 17:53:04 -0300

    Project Creation

---
+++ RunningApp/RunningApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RunningApp
+//
+//  Created by POATEK on 8/29/17.
+//  Copyright © 2017 POATEK. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
